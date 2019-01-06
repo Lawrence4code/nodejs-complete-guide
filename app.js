5752fc1103234fb2e27b3431ec833bd992a55653
Lawrence <Lawrence4code@gmail.com>
2019-01-06 19:41:05 +0530

    integrated ejs along with partial for views

--- app.js
+++ app.js
@@ -2,21 +2,24 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 // this helps in parsing the input to object with key value pair and calls next after parsing
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin/', adminRoutes);
+app.use('/admin/', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', 'pageNotFound.html'));
+  res.status(404).render('404', { pageTitle: 'Page Not found' });
 });
 
 app.listen(3000); // create a unending loop listening to the port.
