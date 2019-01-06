863cadf317905540bca614fecbfb08af397d7d32
Lawrence <Lawrence4code@gmail.com>
2019-01-06 17:14:14 +0530

    added public folder for static css file and created path.js util file for rootDir function

--- app.js
+++ app.js
@@ -1,42 +1,22 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
-
-// app.use((req, res, next) => {
-//   console.log('I am from the first middleware');
-//   next();
-// });
-
-// app.use((req, res, next) => {
-//   console.log(
-//     'I am from the second middleware the will only appear if next is called in my predecessor'
-//   );
-//   res.send('<h1> From the second middleware, awesome right? </h1>');
-// });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 // this helps in parsing the input to object with key value pair and calls next after parsing
 
-app.use('/', (req, res, next) => {
-  console.log('This route always runs');
-  next();
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/products" method="POST"> <input type="text" name="title" > <button type="submit"> Add Product </button> </form>'
-  );
-});
+app.use('/admin/', adminRoutes);
+app.use(shopRoutes);
 
-app.use('/products', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  console.log('I am from the second middleware');
-  res.send('<h1> Hello from express server :) </h1>');
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', 'pageNotFound.html'));
 });
 
 app.listen(3000); // create a unending loop listening to the port.
