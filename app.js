9ab2549ffdab9114071e661f9eb70c6f2a4580fc
Lawrence <Lawrence4code@gmail.com>
2019-01-07 15:21:35 +0530

    Created new routes and views based on project

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -15,11 +17,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin/', adminData.routes);
+app.use('/admin/', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not found' });
-});
+app.use(errorController.get404);
 
 app.listen(3000); // create a unending loop listening to the port.
---
+++ models/product.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const fs = require('fs');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'product.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return cb([]);
+    }
+    cb(JSON.parse(fileContent));
+  });
+};
+
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
+
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+
+    fs.readFile(p, (err, fileContent) => {});
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
