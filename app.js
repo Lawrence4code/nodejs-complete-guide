4cedccbd25c5e78b35dd7381b6c26dd9d4dea619
Lawrence <Lawrence4code@gmail.com>
2019-01-08 12:05:14 +0530

    added card feature and logic for add and delete product and cart product

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
 
 const errorController = require('./controllers/error');
 
@@ -12,14 +10,15 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-// this helps in parsing the input to object with key value pair and calls next after parsing
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin/', adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000); // create a unending loop listening to the port.
+app.listen(3000);
